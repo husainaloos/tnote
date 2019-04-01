35a2be26e6f2011fd259dab00bcae12b3adf2cf4
Husain Alshehhi <husainaloos@users.noreply.github.com>
2019-04-01 16:14:48 -0500

    Add Unit Tests for Creating

--- manager_test.go
+++ manager_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"reflect"
@@ -78,6 +79,60 @@ func TestList(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, test.expected) {
 				t.Errorf("got %v, but expected %v", got, test.expected)
+			}
+		})
+	}
+}
+
+func TestCreate(t *testing.T) {
+	tests := []struct {
+		name     string
+		toCreate string
+		expected []string
+	}{
+		{
+			name:     "testing creating simple file",
+			toCreate: "file",
+			expected: []string{"file.md"},
+		},
+		{
+			name:     "testing simple file with .md",
+			toCreate: "file.md",
+			expected: []string{"file.md.md"},
+		},
+		{
+			name:     "testing simple file with underscore",
+			toCreate: "file_with_underscore",
+			expected: []string{"file_with_underscore.md"},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			if err := os.RemoveAll(testHomeDir); err != nil {
+				t.Fatalf("cannot remove home dir: %v", err)
+			}
+
+			m, _ := NewManager()
+			err := m.setHomeDir(testHomeDir)
+			if err != nil {
+				t.Fatalf("cannot set home dir: %v", err)
+			}
+
+			if err := m.Create(test.toCreate); err != nil {
+				t.Fatalf("failed: %v", err)
+			}
+
+			fis, err := ioutil.ReadDir(testHomeDir)
+			if err != nil {
+				t.Fatalf("cannot read %s: %v", testHomeDir, err)
+			}
+			fns := make([]string, 0)
+			for _, fi := range fis {
+				fns = append(fns, fi.Name())
+			}
+			if !reflect.DeepEqual(fns, test.expected) {
+				t.Fatalf("have %v in dir, but expected %v", fns, test.expected)
 			}
 		})
 	}
